385cf58428a150d429d03bcc1dcc0659d3075d4b
Marcin Szczygliński <61396542+szczyglis-dev@users.noreply.github.com>
2023-04-12 09:58:00 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@
 # GitHub:  https://github.com/szczyglis-dev/py-gpt   #
 # MIT License                                        #
 # Created By  : Marcin Szczygliński                  #
-# Updated Date: 2023.04.12 07:00:00                  #
+# Updated Date: 2023.04.12 09:00:00                  #
 # ================================================== #
 
 __author__ = "Marcin Szczygliński"
