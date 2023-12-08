5da5276076c4571b0fbeef2943747de59da35e91
Marcin Szczyglinski <szczyglis@protonmail.com>
2023-12-08 20:43:07 +0100

    Improved web search

--- src/pygpt_net/core/gpt.py
+++ src/pygpt_net/core/gpt.py
@@ -6,7 +6,7 @@
 # GitHub:  https://github.com/szczyglis-dev/py-gpt   #
 # MIT License                                        #
 # Created By  : Marcin Szczygliński                  #
-# Updated Date: 2023.12.07 10:00:00                  #
+# Updated Date: 2023.12.08 21:00:00                  #
 # ================================================== #
 import base64
 import os
