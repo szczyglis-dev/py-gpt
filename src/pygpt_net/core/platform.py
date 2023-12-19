0ec95110a68ac45c84224e8d653971ed0891b2c2
Marcin Szczyglinski <szczyglis@protonmail.com>
2023-12-19 16:41:26 +0100

    Updated UI

--- src/pygpt_net/core/platform.py
+++ src/pygpt_net/core/platform.py
@@ -6,7 +6,7 @@
 # GitHub:  https://github.com/szczyglis-dev/py-gpt   #
 # MIT License                                        #
 # Created By  : Marcin Szczygliński                  #
-# Updated Date: 2023.12.19 16:00:00                  #
+# Updated Date: 2023.12.19 17:00:00                  #
 # ================================================== #
 
 import platform
