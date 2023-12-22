2715fa55b1194b115890b327e8ba2451f3585179
Marcin Szczyglinski <szczyglis@protonmail.com>
2023-12-22 03:20:14 +0100

    Added new ctx in menu

--- src/pygpt_net/core/controller/lang.py
+++ src/pygpt_net/core/controller/lang.py
@@ -6,7 +6,7 @@
 # GitHub:  https://github.com/szczyglis-dev/py-gpt   #
 # MIT License                                        #
 # Created By  : Marcin Szczygliński                  #
-# Updated Date: 2023.12.21 07:00:00                  #
+# Updated Date: 2023.12.22 03:00:00                  #
 # ================================================== #
 
 from PySide6.QtGui import QAction, Qt
