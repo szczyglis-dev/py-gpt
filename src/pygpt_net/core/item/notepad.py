3e8b2c6a8c2b4c39182d65f5c6c0d9312cd3cb02
Marcin Szczyglinski <szczyglis@protonmail.com>
2023-12-24 18:49:06 +0100

    Added default created at

--- src/pygpt_net/core/item/notepad.py
+++ src/pygpt_net/core/item/notepad.py
@@ -8,12 +8,15 @@
 # Created By  : Marcin Szczygliński                  #
 # Updated Date: 2023.12.23 19:00:00                  #
 # ================================================== #
+import datetime
 
 
 class NotepadItem:
     def __init__(self):
-        self.id = None
+        self.id = 0
         self.title = ""
         self.content = ""
-        self.created_at = None
-        self.updated_at = None
+
+        dt = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+        self.created_at = dt
+        self.updated_at = dt
