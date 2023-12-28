9852f9fb91e24d53ad738848acb33f1d5ae97626
Marcin Szczyglinski <szczyglis@protonmail.com>
2023-12-28 17:39:05 +0100

    Removed unused import

--- src/pygpt_net/core/ctx.py
+++ src/pygpt_net/core/ctx.py
@@ -6,14 +6,13 @@
 # GitHub:  https://github.com/szczyglis-dev/py-gpt   #
 # MIT License                                        #
 # Created By  : Marcin Szczygliński                  #
-# Updated Date: 2023.12.25 21:00:00                  #
+# Updated Date: 2023.12.28 17:00:00                  #
 # ================================================== #
 
 import datetime
 import os
 
 from pygpt_net.item.ctx import CtxItem, CtxMeta
-from pygpt_net.provider.ctx.json_file import JsonFileProvider
 from pygpt_net.provider.ctx.db_sqlite import DbSqliteProvider
 from pygpt_net.utils import trans
 
