530a02aba03e1b997c85ff584df87337166c9888
Marcin Szczyglinski <szczyglis@protonmail.com>
2024-01-04 09:49:03 +0100

    Added password widget

--- src/pygpt_net/ui/widget/option/input.py
+++ src/pygpt_net/ui/widget/option/input.py
@@ -8,7 +8,7 @@
 # Created By  : Marcin Szczygliński                  #
 # Updated Date: 2023.12.25 21:00:00                  #
 # ================================================== #
-
+from PySide6.QtGui import QAction
 from PySide6.QtWidgets import QLineEdit
 
 
@@ -60,6 +60,53 @@ class OptionInput(QLineEdit):
         :param event: key event
         """
         super(OptionInput, self).keyPressEvent(event)
+        if not self.autoupdate:
+            return
+        self.window.controller.ui.update()
+        self.window.controller.settings.editor.change(self.id, self.text(), self.section)
+
+
+class PasswordInput(QLineEdit):
+    def __init__(self, window=None, id=None, autoupdate=False, section=None):
+        """
+        Settings input
+
+        :param window: main window
+        :param id: option id
+        :param autoupdate: auto update
+        :param section: settings section
+        """
+        super(PasswordInput, self).__init__(window)
+        self.window = window
+        self.id = id
+        self.section = section
+        self.autoupdate = autoupdate
+
+        self.setEchoMode(QLineEdit.Password)
+
+        self.toggle_password_action = QAction('+', self)
+        self.toggle_password_action.triggered.connect(self.toggle_password_visibility)
+        self.addAction(self.toggle_password_action, QLineEdit.TrailingPosition)
+
+        self.is_password_shown = False
+
+    def toggle_password_visibility(self):
+        if self.is_password_shown:
+            self.setEchoMode(QLineEdit.Password)
+            self.toggle_password_action.setText('+')
+            self.is_password_shown = False
+        else:
+            self.setEchoMode(QLineEdit.Normal)
+            self.toggle_password_action.setText('-')
+            self.is_password_shown = True
+
+    def keyPressEvent(self, event):
+        """
+        Key press event
+
+        :param event: key event
+        """
+        super(PasswordInput, self).keyPressEvent(event)
         if not self.autoupdate:
             return
         self.window.controller.ui.update()
