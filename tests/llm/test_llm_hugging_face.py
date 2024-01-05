faf59d1ec556bbe4ddb3a00f2509e26f01be3e3b
Marcin Szczyglinski <szczyglis@protonmail.com>
2024-01-05 04:14:38 +0100

    Added LLM-wrappers tests templates

---
+++ tests/llm/test_llm_hugging_face.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ================================================== #
+# This file is a part of PYGPT package               #
+# Website: https://pygpt.net                         #
+# GitHub:  https://github.com/szczyglis-dev/py-gpt   #
+# MIT License                                        #
+# Created By  : Marcin Szczygliński                  #
+# Updated Date: 2024.01.05 03:00:00                  #
+# ================================================== #
+
+from unittest.mock import MagicMock, patch, mock_open, Mock
+
+from tests.mocks import mock_window
+from pygpt_net.llm.HuggingFace import HuggingFaceLLM as Wrapper
+
+
+def test_completion(mock_window):
+    config = mock_window.core.config
+    options = {}
+    stream = False
+    wrapper = Wrapper()
+    wrapper.completion = MagicMock(return_value=Mock())
+    wrapper.completion(config, options, stream)
+    wrapper.completion.assert_called_once()
+
+
+def test_chat(mock_window):
+    config = mock_window.core.config
+    options = {}
+    stream = False
+    wrapper = Wrapper()
+    wrapper.chat = MagicMock(return_value=Mock())
+    wrapper.chat(config, options, stream)
+    wrapper.chat.assert_called_once()
+    
