db894e16cf09572e9cc5b7018113bb3ed5c2f378
Marcin Szczyglinski <szczyglis@protonmail.com>
2024-01-07 09:56:20 +0100

    Added label field

--- src/pygpt_net/item/ctx.py
+++ src/pygpt_net/item/ctx.py
@@ -6,7 +6,7 @@
 # GitHub:  https://github.com/szczyglis-dev/py-gpt   #
 # MIT License                                        #
 # Created By  : Marcin Szczygliński                  #
-# Updated Date: 2023.12.31 04:00:00                  #
+# Updated Date: 2024.01.07 10:00:00                  #
 # ================================================== #
 
 import datetime
@@ -153,3 +153,4 @@ class CtxMeta:
         self.deleted = False
         self.important = False
         self.archived = False
+        self.label = 0  # label color
