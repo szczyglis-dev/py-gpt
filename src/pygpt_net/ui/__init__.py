c01afb0c3188e752f1350ada4f48e607ce5d3c63
Marcin Szczyglinski <szczyglis@protonmail.com>
2024-01-08 19:11:49 +0100

    Updated header

--- src/pygpt_net/ui/__init__.py
+++ src/pygpt_net/ui/__init__.py
@@ -6,7 +6,7 @@
 # GitHub:  https://github.com/szczyglis-dev/py-gpt   #
 # MIT License                                        #
 # Created By  : Marcin Szczygliński                  #
-# Updated Date: 2023.12.28 16:00:00                  #
+# Updated Date: 2024.01.08 17:00:00                  #
 # ================================================== #
 
 import os
