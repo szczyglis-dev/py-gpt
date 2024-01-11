5d0c233b9caafd72663ca51ad77a21971266ecc3
Marcin Szczyglinski <szczyglis@protonmail.com>
2024-01-11 16:40:21 +0100

    Added missing __init__

---
+++ src/pygpt_net/ui/widget/draw/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ================================================== #
+# This file is a part of PYGPT package               #
+# Website: https://pygpt.net                         #
+# GitHub:  https://github.com/szczyglis-dev/py-gpt   #
+# MIT License                                        #
+# Created By  : Marcin Szczygliński                  #
+# Updated Date: 2024.01.11 17:00:00                  #
+# ================================================== #
