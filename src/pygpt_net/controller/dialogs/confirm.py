5595220d5cca1ae9c53210ff571df1fa44d39dcf
Marcin Szczyglinski <szczyglis@protonmail.com>
2024-01-14 15:11:58 +0100

    Added handles for model editor

--- src/pygpt_net/controller/dialogs/confirm.py
+++ src/pygpt_net/controller/dialogs/confirm.py
@@ -6,7 +6,7 @@
 # GitHub:  https://github.com/szczyglis-dev/py-gpt   #
 # MIT License                                        #
 # Created By  : Marcin Szczygliński                  #
-# Updated Date: 2024.01.12 21:00:00                  #
+# Updated Date: 2024.01.13 06:00:00                  #
 # ================================================== #
 
 class Confirm:
@@ -28,44 +28,69 @@ class Confirm:
         """
         self.window.ui.dialog['confirm'].close()
 
+        # presets
         if type == 'preset_exists':
             self.window.controller.presets.editor.save(True)
         elif type == 'preset_delete':
             self.window.controller.presets.delete(id, True)
         elif type == 'preset_clear':
             self.window.controller.presets.clear(True)
+
+        # ctx
         elif type == 'ctx_delete':
             self.window.controller.ctx.delete(id, True)
         elif type == 'ctx_delete_all':
             self.window.controller.ctx.delete_history(True)
+
+        # images
         elif type == 'img_delete':
             self.window.controller.chat.image.delete(id, True)
+
+        # attachments
         elif type == 'attachments.delete':
             self.window.controller.attachment.delete(id, True)
+        elif type == 'attachments_uploaded.clear':
+            self.window.controller.assistant.files.clear_files(True)
         elif type == 'attachments_uploaded.delete':
             self.window.controller.assistant.files.delete(id, True)
         elif type == 'attachments.clear':
             self.window.controller.attachment.clear(True)
-        elif type == 'attachments_uploaded.clear':
-            self.window.controller.assistant.files.clear_files(True)
+
+        # files
         elif type == 'files.delete':
             self.window.controller.files.delete(id, True)
+
+        # assistants
         elif type == 'assistant_delete':
             self.window.controller.assistant.delete(id, True)
         elif type == 'assistant_import':
             self.window.controller.assistant.import_api(True)
         elif type == 'assistant_import_files':
             self.window.controller.assistant.files.sync(True)
+
+        # settings
         elif type == 'settings.defaults.user':
             self.window.controller.settings.editor.load_defaults_user(True)
         elif type == 'settings.defaults.app':
             self.window.controller.settings.editor.load_defaults_app(True)
         elif type == 'settings.dict.delete':
             self.window.controller.config.dictionary.delete_item(parent_object, id, True)
+
+        # plugins
         elif type == 'plugin.settings.defaults.user':
             self.window.controller.plugins.settings.load_defaults_user(True)
         elif type == 'plugin.settings.defaults.app':
             self.window.controller.plugins.settings.load_defaults_app(True)
+
+        # models
+        elif type == 'models.editor.delete':
+            self.window.controller.model.editor.delete_by_idx(id, True)
+        elif type == 'models.editor.defaults.user':
+            self.window.controller.model.editor.load_defaults_user(True)
+        elif type == 'models.editor.defaults.app':
+            self.window.controller.model.editor.load_defaults_app(True)
+
+        # index
         elif type == 'idx.index.files.all':
             self.window.controller.idx.indexer.index_all_files(id, True)
         elif type == 'idx.index.db.all':
