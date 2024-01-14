4717df58f69b71c58d4b6544a19e983d6ca42a82
Marcin Szczyglinski <szczyglis@protonmail.com>
2024-01-14 15:23:44 +0100

    Added patch for new format for langchain/llama

--- src/pygpt_net/provider/model/patch.py
+++ src/pygpt_net/provider/model/patch.py
@@ -55,13 +55,59 @@ class Patch:
                 data = self.window.core.models.items
                 updated = True
 
-            # < 2.0.103  <--- patch for llama-index gpt4-turbo
-            if old < parse_version("2.0.103"):
-                print("Migrating models from < 2.0.103...")
+            # < 2.0.104  <--- patch for llama-index gpt4-turbo
+            if old < parse_version("2.0.104"):
+                print("Migrating models from < 2.0.104...")
                 self.window.core.updater.patch_file('models.json', True)  # force replace file
                 self.window.core.models.load()
                 data = self.window.core.models.items
                 updated = True
+
+            '''
+            # < 2.0.104  <--- patch to new format
+            if old < parse_version("2.0.104"):
+                print("Migrating models from < 2.0.104...")
+                for id in data:
+                    model = data[id]
+                    dict_name = model.to_dict()
+                    model.from_dict(dict_name)
+
+                    # patch missing llama_index provider
+                    if "llama_index" in model.mode:
+                        if model.id.startswith("gpt-") or model.id.startswith("text-davinci-"):
+                            model.llama_index["provider"] = "openai"
+                            if model.id.startswith("gpt-"):
+                                model.llama_index['mode'] = ["chat"]
+                            model.llama_index['args'] = [
+                                {
+                                    "name": "model_name",
+                                    "value": model.id,
+                                    "type": "str",
+                                }
+                            ]
+                            model.llama_index['env'] = [
+                                {
+                                    "name": "OPENAI_API_KEY",
+                                    "value": "{api_key}",
+                                }
+                            ]
+                    if "langchain" in model.mode:
+                        if model.id.startswith("gpt-") or model.id.startswith("text-davinci-"):
+                            model.langchain['args'] = [
+                                {
+                                    "name": "model_name",
+                                    "value": model.id,
+                                    "type": "str",
+                                }
+                            ]
+                            model.langchain['env'] = [
+                                {
+                                    "name": "OPENAI_API_KEY",
+                                    "value": "{api_key}",
+                                }
+                            ]
+                updated = True
+            '''
 
         # update file
         if updated:
