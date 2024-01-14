8788425092a48e5a74ee446d80ac91b33d200a22
Marcin Szczyglinski <szczyglis@protonmail.com>
2024-01-14 15:14:48 +0100

    Added model editor

--- src/pygpt_net/ui/menu.py
+++ src/pygpt_net/ui/menu.py
@@ -97,10 +97,8 @@ class Menu:
         """Setup config menu"""
         self.window.ui.menu['config.settings'] = QAction(QIcon.fromTheme("preferences-other"),
                                                          trans("menu.config.settings"), self.window)
-        self.window.ui.menu['config.edit.config'] = QAction(QIcon.fromTheme("document-edit"),
-                                                            trans("menu.config.edit.config"), self.window)
-        self.window.ui.menu['config.edit.models'] = QAction(QIcon.fromTheme("document-edit"),
-                                                            trans("menu.config.edit.models"), self.window)
+        self.window.ui.menu['config.models'] = QAction(QIcon.fromTheme("preferences-other"),
+                                                         trans("menu.config.models"), self.window)
 
         css_files = []
         css_files.append("style.css")
@@ -110,8 +108,18 @@ class Menu:
         css_files.append("markdown.dark.css")
         css_files.append("markdown.light.css")
 
+        json_files = []
+        json_files.append("attachments.json")
+        json_files.append("assistants.json")
+        json_files.append("config.json")
+        json_files.append("indexes.json")
+        json_files.append("models.json")
+
         # create submenu for css files
         self.window.ui.menu['config.edit.css'] = QMenu(trans("menu.config.edit.css"), self.window)
+
+        # create submenu for JSON files
+        self.window.ui.menu['config.edit.json'] = QMenu(trans("menu.config.edit.json"), self.window)
 
         for css_file in css_files:
             name = css_file.split("/")[-1]
@@ -121,6 +129,14 @@ class Menu:
                 lambda checked=True, file=css_file: self.window.controller.settings.toggle_file_editor(file))
             self.window.ui.menu['config.edit.css'].addAction(self.window.ui.menu['config.edit.css.' + name])
 
+        for json_file in json_files:
+            name = json_file
+            self.window.ui.menu['config.edit.json.' + name] = QAction(QIcon.fromTheme("document-edit"),
+                                                                name, self.window)
+            self.window.ui.menu['config.edit.json.' + name].triggered.connect(
+                lambda checked=True, file=json_file: self.window.controller.settings.toggle_file_editor(file))
+            self.window.ui.menu['config.edit.json'].addAction(self.window.ui.menu['config.edit.json.' + name])
+
         self.window.ui.menu['config.open_dir'] = QAction(QIcon.fromTheme("folder-open"),
                                                          trans("menu.config.open_dir"), self.window)
         self.window.ui.menu['config.save'] = QAction(QIcon.fromTheme("document-save"),
@@ -129,11 +145,8 @@ class Menu:
         self.window.ui.menu['config.settings'].triggered.connect(
             lambda: self.window.controller.settings.toggle_editor('settings'))
 
-        self.window.ui.menu['config.edit.config'].triggered.connect(
-            lambda: self.window.controller.settings.toggle_file_editor('config.json'))
-
-        self.window.ui.menu['config.edit.models'].triggered.connect(
-            lambda: self.window.controller.settings.toggle_file_editor('models.json'))
+        self.window.ui.menu['config.models'].triggered.connect(
+            lambda: self.window.controller.model.editor.toggle_editor())
 
         self.window.ui.menu['config.open_dir'].triggered.connect(
             lambda: self.window.controller.settings.open_config_dir())
@@ -143,9 +156,9 @@ class Menu:
 
         self.window.ui.menu['menu.config'] = self.window.menuBar().addMenu(trans("menu.config"))
         self.window.ui.menu['menu.config'].addAction(self.window.ui.menu['config.settings'])
-        self.window.ui.menu['menu.config'].addAction(self.window.ui.menu['config.edit.config'])
-        self.window.ui.menu['menu.config'].addAction(self.window.ui.menu['config.edit.models'])
+        self.window.ui.menu['menu.config'].addAction(self.window.ui.menu['config.models'])
         self.window.ui.menu['menu.config'].addMenu(self.window.ui.menu['config.edit.css'])
+        self.window.ui.menu['menu.config'].addMenu(self.window.ui.menu['config.edit.json'])
         self.window.ui.menu['menu.config'].addAction(self.window.ui.menu['config.open_dir'])
         self.window.ui.menu['menu.config'].addAction(self.window.ui.menu['config.save'])
 
