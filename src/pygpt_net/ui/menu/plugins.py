5ea72f093c322a7bae006a0fd3aa355c1d0fbf07
Marcin Szczyglinski <szczyglis@protonmail.com>
2024-01-18 10:21:13 +0100

    Added tooltips to plugins menu - issue #7

--- src/pygpt_net/ui/menu/plugins.py
+++ src/pygpt_net/ui/menu/plugins.py
@@ -6,7 +6,7 @@
 # GitHub:  https://github.com/szczyglis-dev/py-gpt   #
 # MIT License                                        #
 # Created By  : Marcin Szczygliński                  #
-# Updated Date: 2024.01.15 05:00:00                  #
+# Updated Date: 2024.01.18 10:00:00                  #
 # ================================================== #
 
 from PySide6.QtGui import QAction, QIcon
