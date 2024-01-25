191a9dcccdc3d9d90ea908da9635f1ae9fd4500f
Marcin Szczyglinski <szczyglis@protonmail.com>
2024-01-25 22:42:07 +0100

    Added finished signal

--- src/pygpt_net/core/worker.py
+++ src/pygpt_net/core/worker.py
@@ -6,7 +6,7 @@
 # GitHub:  https://github.com/szczyglis-dev/py-gpt   #
 # MIT License                                        #
 # Created By  : Marcin Szczygliński                  #
-# Updated Date: 2023.12.31 04:00:00                  #
+# Updated Date: 2024.01.25 19:00:00                  #
 # ================================================== #
 
 from PySide6.QtCore import QRunnable, Slot, QObject, Signal
@@ -26,3 +26,4 @@ class Worker(QRunnable):
 
 class WorkerSignals(QObject):
     updated = Signal(object)
+    finished = Signal(object)
