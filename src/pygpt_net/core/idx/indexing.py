2a28beaed88863719a7411addb0c18ecd9665fe4
Marcin Szczyglinski <szczyglis@protonmail.com>
2024-02-03 17:59:34 +0100

    Updated version to 2.0.146

--- src/pygpt_net/core/idx/indexing.py
+++ src/pygpt_net/core/idx/indexing.py
@@ -6,7 +6,7 @@
 # GitHub:  https://github.com/szczyglis-dev/py-gpt   #
 # MIT License                                        #
 # Created By  : Marcin Szczygliński                  #
-# Updated Date: 2024.02.05 18:00:00                  #
+# Updated Date: 2024.02.03 18:00:00                  #
 # ================================================== #
 
 import os.path
