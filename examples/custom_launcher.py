e97ddf0a9d8a4f0d6739f233c7123fcaf6298676
Marcin Szczyglinski <szczyglis@protonmail.com>
2024-02-19 23:17:13 +0100

    Updated comments

--- examples/custom_launcher.py
+++ examples/custom_launcher.py
@@ -1,6 +1,6 @@
 # PyGPT custom launcher example.
 
-from pygpt_net.app import run  # <-- import the run function from the app
+from pygpt_net.app import run  # <-- import the "run" function from the app
 
 from example_plugin import Plugin as ExamplePlugin
 from example_llm import ExampleLlm
@@ -8,26 +8,28 @@ from example_vector_store import ExampleVectorStore
 from example_data_loader import ExampleDataLoader
 
 """
-Extending PyGPT with custom plugins, LLMs wrappers, vector stores and data loaders
+Extending PyGPT with custom plugins, LLM wrappers, vector stores, and data loaders:
 
-- You can pass custom plugin instances, LLMs wrappers and vector store providers to the launcher.
-- This is useful if you want to extend PyGPT with your own plugins, vectors storage and LLMs.
+- You can pass custom plugin, LLM wrappers, vector store providers, and data loader instances to the launcher.
+- This is useful if you want to extend PyGPT with your own plugins, vectors storage, data loaders, or LLMs.
 
-To register custom plugins create custom launcher, e.g. "my_launcher.py" and:
+First, create a custom launcher file, for example, "my_launcher.py," and register your extensions in it.
 
-- Pass a list with the plugin instances as 'plugins' keyword argument.
+To register custom plugin:
 
-To register custom LLMs wrappers:
+- Pass a list with the plugin instances as the "plugins" keyword argument.
 
-- Pass a list with the LLMs wrappers instances as 'llms' keyword argument.
+To register custom LLM wrapper:
 
-To register custom vector store providers:
+- Pass a list with the LLM wrapper instances as the "llms" keyword argument.
 
-- Pass a list with the vector store provider instances as 'vector_stores' keyword argument.
+To register custom vector store provider:
 
-To register custom data loaders:
+- Pass a list with the vector store provider instances as the "vector_stores" keyword argument.
 
-- Pass a list with the data loader instances as 'loaders' keyword argument.
+To register custom data loader:
+
+- Pass a list with the data loader instances as the "loaders" keyword argument.
 
 Example:
 --------
@@ -66,28 +68,28 @@ Example:
 
 # Working example:
 
-# 1. Prepare instances of your custom plugins, LLMs, vector stores and data loaders:
+# 1. Prepare instances of your custom plugins, LLMs, vector stores, and data loaders:
 
 plugins = [
     ExamplePlugin(),  # add your custom plugin instances here
 ]
 llms = [
-    ExampleLlm(),  # add your custom LLM instances here
+    ExampleLlm(),  # add your custom LLM wrappers instances here
 ]
 vector_stores = [
     ExampleVectorStore(),  # add your custom vector store instances here
 ]
 loaders = [
-    ExampleDataLoader(),
+    ExampleDataLoader(),  # add your custom data loader instances here
 ]
 
-# 2. Register example plugins, LLMs, vector stores and data loaders to the PyGPT launcher using the run function:
+# 2. Register example plugins, LLM wrappers, vector stores, and data loaders using the run function:
 
 run(
     plugins=plugins,  # pass the list with the plugin instances
     llms=llms,  # pass the list with the LLM provider instances
     vector_stores=vector_stores,  # pass the list with the vector store instances
-    loaders=loaders  # pass the list with the data loader instances
+    loaders=loaders,  # pass the list with the data loader instances
 )
 
 if __name__ == '__main__':
