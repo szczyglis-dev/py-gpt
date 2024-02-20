4ff8faf13ac4d1238a5cbd9fe6335e7282eda6d1
Marcin Szczyglinski <szczyglis@protonmail.com>
2024-02-20 03:02:33 +0100

    Revert "Updated description"
    This reverts commit a7f35954b8661fbfc2b1b0797bba0c44f65e192b.

--- examples/example_plugin.py
+++ examples/example_plugin.py
@@ -54,8 +54,6 @@ class Plugin(BasePlugin):
         """
         Handle dispatched event here
 
-        This method is called externally by event dispatcher every time when the event is dispatched.
-
         :param event: event object
         :param args: event args
         :param kwargs: event kwargs
