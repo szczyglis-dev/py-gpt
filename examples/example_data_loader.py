c1aec499a64198555bcb1ec08ad1930b5105e27c
Marcin Szczyglinski <szczyglis@protonmail.com>
2024-02-28 04:57:37 +0100

    Updated with llama-index 0.10 import

--- examples/example_data_loader.py
+++ examples/example_data_loader.py
@@ -6,14 +6,14 @@
 # GitHub:  https://github.com/szczyglis-dev/py-gpt   #
 # MIT License                                        #
 # Created By  : Marcin Szczygliński                  #
-# Updated Date: 2024.02.27 05:00:00                  #
+# Updated Date: 2024.02.28 05:00:00                  #
 # ================================================== #
 
 from pathlib import Path
 from typing import Any, Dict, List, Optional
 
-from llama_index.readers.schema.base import Document
-from llama_index.readers.base import BaseReader
+from llama_index.core import Document
+from llama_index.core.readers.base import BaseReader
 
 from pygpt_net.provider.loaders.base import BaseLoader  # <--- data loader must inherit from BaseLoader
 
--- examples/example_vector_store.py
+++ examples/example_vector_store.py
@@ -6,17 +6,15 @@
 # GitHub:  https://github.com/szczyglis-dev/py-gpt   #
 # MIT License                                        #
 # Created By  : Marcin Szczygliński                  #
-# Updated Date: 2024.02.20 02:00:00                  #
+# Updated Date: 2024.02.28 02:00:00                  #
 # ================================================== #
 
 import os.path
-from llama_index.indices.base import BaseIndex
-from llama_index import (
-    VectorStoreIndex,
-    StorageContext,
-    ServiceContext,
-    load_index_from_storage,
-)
+
+from llama_index.core import StorageContext, load_index_from_storage
+from llama_index.core.indices.base import BaseIndex
+from llama_index.core.indices.service_context import ServiceContext
+from llama_index.core.indices.vector_store.base import VectorStoreIndex
 
 from pygpt_net.provider.vector_stores.base import BaseStore  # <--- vector store must inherit from BaseStore
 
