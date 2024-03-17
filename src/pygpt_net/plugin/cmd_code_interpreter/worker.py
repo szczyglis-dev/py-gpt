2e3945a083a8512318e3cde1b5ad636ab77c9001
Marcin Szczyglinski <szczyglis@protonmail.com>
2024-03-17 09:15:06 +0100

    Added getting current data, output or code

--- src/pygpt_net/plugin/cmd_code_interpreter/worker.py
+++ src/pygpt_net/plugin/cmd_code_interpreter/worker.py
@@ -6,7 +6,7 @@
 # GitHub:  https://github.com/szczyglis-dev/py-gpt   #
 # MIT License                                        #
 # Created By  : Marcin Szczygliński                  #
-# Updated Date: 2024.03.16 15:00:00                  #
+# Updated Date: 2024.03.17 09:00:00                  #
 # ================================================== #
 
 from PySide6.QtCore import Slot, Signal
@@ -208,7 +208,7 @@ class Worker(BaseWorker):
         """
         request = self.prepare_request(item)
         try:
-            output = self.plugin.window.controller.interpreter.get_output()
+            output = self.plugin.window.controller.interpreter.get_data()
             response = {
                 "request": request,
                 "result": output,
