fa8d4f978482a7d39d470d89a4d71607f5960936
Marcin Szczyglinski <szczyglis@protonmail.com>
2024-03-19 04:34:14 +0100

    Updated version to 2.1.36

--- src/pygpt_net/controller/layout.py
+++ src/pygpt_net/controller/layout.py
@@ -19,7 +19,7 @@ class Layout:
         """
         self.window = window
         # self.splitters = ["main", "main.output", "toolbox", "toolbox.mode", "toolbox.presets"]
-        self.splitters = ["main", "main.output", "toolbox", "toolbox.mode", "calendar", "interpreter"]
+        self.splitters = ["main", "main.output", "toolbox", "toolbox.mode", "calendar", "interpreter", "interpreter.columns"]
         self.text_nodes = ["input"]
 
     def setup(self):
