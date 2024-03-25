58d848e9d0e3a95b841d1f736e04807e6b3e5d66
Marcin Szczyglinski <szczyglis@protonmail.com>
2024-03-25 16:54:53 +0100

    Reload after migrate moved to controller

--- src/pygpt_net/ui/dialog/workdir.py
+++ src/pygpt_net/ui/dialog/workdir.py
@@ -6,7 +6,7 @@
 # GitHub:  https://github.com/szczyglis-dev/py-gpt   #
 # MIT License                                        #
 # Created By  : Marcin Szczygliński                  #
-# Updated Date: 2024.02.25 12:00:00                  #
+# Updated Date: 2024.03.25 12:00:00                  #
 # ================================================== #
 
 from PySide6.QtCore import Qt
