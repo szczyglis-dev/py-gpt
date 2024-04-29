4c00db2c020b718145615175cd01101fe4ab2e2e
Marcin Szczyglinski <szczyglis@protonmail.com>
2024-04-29 07:00:56 +0200

    Update list added on mode select

--- src/pygpt_net/controller/mode.py
+++ src/pygpt_net/controller/mode.py
@@ -6,7 +6,7 @@
 # GitHub:  https://github.com/szczyglis-dev/py-gpt   #
 # MIT License                                        #
 # Created By  : Marcin Szczygliński                  #
-# Updated Date: 2024.03.15 15:00:00                  #
+# Updated Date: 2024.04.29 07:00:00                  #
 # ================================================== #
 
 from pygpt_net.core.dispatcher import Event
@@ -40,6 +40,7 @@ class Mode:
             'value': mode,
         })
         self.window.core.dispatcher.dispatch(event)
+        self.window.controller.attachment.update()
 
     def set(self, mode: str):
         """
