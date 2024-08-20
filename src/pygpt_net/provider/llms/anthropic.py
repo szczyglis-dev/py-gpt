fe24b96c536d9a60b32a28c00eff7c73b8ecef10
Marcin Szczyglinski <szczyglis@protonmail.com>
2024-08-20 18:32:46 +0200

    Added config for Llama index

--- src/pygpt_net/provider/llms/anthropic.py
+++ src/pygpt_net/provider/llms/anthropic.py
@@ -6,12 +6,12 @@
 # GitHub:  https://github.com/szczyglis-dev/py-gpt   #
 # MIT License                                        #
 # Created By  : Marcin Szczygliński                  #
-# Updated Date: 2024.01.14 11:00:00                  #
+# Updated Date: 2024.08.20 16:00:00                  #
 # ================================================== #
 
-from langchain_community.llms import Anthropic
-from langchain_community.chat_models import ChatAnthropic
+from llama_index.llms.anthropic import Anthropic
 
+from llama_index.core.llms.llm import BaseLLM as LlamaBaseLLM
 from pygpt_net.provider.llms.base import BaseLLM
 from pygpt_net.item.model import ModelItem
 
@@ -19,29 +19,24 @@ from pygpt_net.item.model import ModelItem
 class AnthropicLLM(BaseLLM):
     def __init__(self, *args, **kwargs):
         super(AnthropicLLM, self).__init__(*args, **kwargs)
+        """
+        Required ENV variables:
+            - ANTHROPIC_API_KEY - API key for Anthropic API
+        Required args:
+            - model: model name, e.g. claude-3-opus-20240229
+            - api_key: API key for Anthropic API
+        """
         self.id = "anthropic"
-        self.type = ["langchain"]
+        self.type = ["llama_index"]
 
-    def completion(self, window, model: ModelItem, stream: bool = False):
+    def llama(self, window, model: ModelItem, stream: bool = False) -> LlamaBaseLLM:
         """
-        Return LLM provider instance for completion
+        Return LLM provider instance for llama
 
         :param window: window instance
         :param model: model instance
         :param stream: stream mode
         :return: LLM provider instance
         """
-        args = self.parse_args(model.langchain)
+        args = self.parse_args(model.llama_index)
         return Anthropic(**args)
-
-    def chat(self, window, model: ModelItem, stream: bool = False):
-        """
-        Return LLM provider instance for chat
-
-        :param window: window instance
-        :param model: model instance
-        :param stream: stream mode
-        :return: LLM provider instance
-        """
-        args = self.parse_args(model.langchain)
-        return ChatAnthropic(**args)
