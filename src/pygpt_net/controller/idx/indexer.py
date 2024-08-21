c939d0669eab522bcaad9ab8335374f3854cd1ca
Marcin Szczyglinski <szczyglis@protonmail.com>
2024-08-21 18:51:00 +0200

    Updated header

--- src/pygpt_net/controller/idx/indexer.py
+++ src/pygpt_net/controller/idx/indexer.py
@@ -6,7 +6,7 @@
 # GitHub:  https://github.com/szczyglis-dev/py-gpt   #
 # MIT License                                        #
 # Created By  : Marcin Szczygliński                  #
-# Updated Date: 2024.04.17 01:00:00                  #
+# Updated Date: 2024.08.21 19:00:00                  #
 # ================================================== #
 
 import datetime
