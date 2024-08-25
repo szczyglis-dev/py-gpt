f10acb3403477060d7f79163bb5b220f1a19b769
Marcin Szczyglinski <szczyglis@protonmail.com>
2024-08-25 20:27:52 +0200

    Added missing environment activate

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # This script is used to install the app dependencies and run the app using the virtual environment
 python -m venv ./venv
+source ./venv/bin/activate
 pip install -r requirements.txt
 python3 run.py "$@"
