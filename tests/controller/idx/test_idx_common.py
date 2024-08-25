6063d92c74dfb6d3a208a64f74710fb17e6584be
Marcin Szczyglinski <szczyglis@protonmail.com>
2024-08-25 05:29:54 +0200

    Removed unused tests

--- tests/controller/idx/test_idx_common.py
+++ tests/controller/idx/test_idx_common.py
@@ -6,7 +6,7 @@
 # GitHub:  https://github.com/szczyglis-dev/py-gpt   #
 # MIT License                                        #
 # Created By  : Marcin Szczygliński                  #
-# Updated Date: 2024.01.23 19:00:00                  #
+# Updated Date: 2024.08.25 06:00:00                  #
 # ================================================== #
 
 from unittest.mock import MagicMock
@@ -17,35 +17,4 @@ from pygpt_net.controller.idx.common import Common
 
 def test_setup(mock_window):
     """Test setup"""
-    common = Common(mock_window)
-    mock_window.ui.config['global']['llama.idx.raw'].setChecked = MagicMock()
-    common.setup()
-    mock_window.ui.config['global']['llama.idx.raw'].setChecked.assert_called_once()
-
-
-def test_enable_raw(mock_window):
-    """Test enable raw"""
-    common = Common(mock_window)
-    mock_window.core.config.set = MagicMock()
-    mock_window.core.config.save = MagicMock()
-    common.enable_raw()
-    mock_window.core.config.set.assert_called_once()
-    mock_window.core.config.save.assert_called_once()
-
-
-def test_disable_raw(mock_window):
-    """Test disable raw"""
-    common = Common(mock_window)
-    mock_window.core.config.set = MagicMock()
-    mock_window.core.config.save = MagicMock()
-    common.disable_raw()
-    mock_window.core.config.set.assert_called_once()
-    mock_window.core.config.save.assert_called_once()
-
-
-def test_toggle_raw(mock_window):
-    """Test toggle raw"""
-    common = Common(mock_window)
-    common.enable_raw = MagicMock()
-    common.toggle_raw(True)
-    common.enable_raw.assert_called_once()
+    pass
