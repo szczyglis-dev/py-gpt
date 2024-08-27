af74d81644c417d614adeaf2a5fc4ba4a9965b6a
Marcin Szczyglinski <szczyglis@protonmail.com>
2024-08-27 23:36:40 +0200

    Added plugin events

--- src/pygpt_net/core/debug/events.py
+++ src/pygpt_net/core/debug/events.py
@@ -6,10 +6,11 @@
 # GitHub:  https://github.com/szczyglis-dev/py-gpt   #
 # MIT License                                        #
 # Created By  : Marcin Szczygliński                  #
-# Updated Date: 2024.05.05 12:00:00                  #
+# Updated Date: 2024.08.27 22:00:00                  #
 # ================================================== #
 
 from pygpt_net.core.access.events import ControlEvent, AppEvent
+from pygpt_net.core.dispatcher import Event
 
 
 class EventsDebug:
@@ -27,6 +28,18 @@ class EventsDebug:
         self.window.core.debug.begin(self.id)
         self.window.core.debug.add(self.id, 'App Events:', str(AppEvent.__dict__))
         self.window.core.debug.add(self.id, 'Control Events:', str(ControlEvent.__dict__))
+        self.window.core.debug.add(self.id, 'Plugin Events:', str(self.extract_plugins()))
         self.window.core.debug.add(self.id, 'Voice Cmds (all):', str(self.window.core.access.voice.commands))
         self.window.core.debug.add(self.id, 'Voice Cmds (allowed):', str(self.window.core.access.voice.get_commands()))
         self.window.core.debug.end(self.id)
+
+    def extract_plugins(self) -> dict:
+        """Extract plugin events"""
+        result = {}
+        for property, value in vars(Event).items():
+            if (isinstance(value, str)
+                    or isinstance(value, int)
+                    or isinstance(value, float)
+                    or isinstance(value, bool)):
+                result[property] = value
+        return result
