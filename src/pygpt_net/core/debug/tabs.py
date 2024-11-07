71de52afe37b4419229a034a93184ac26ce8f544
Marcin Szczyglinski <szczyglis@protonmail.com>
2024-11-08 00:33:42 +0100

    Added current state debug

--- src/pygpt_net/core/debug/tabs.py
+++ src/pygpt_net/core/debug/tabs.py
@@ -6,7 +6,7 @@
 # GitHub:  https://github.com/szczyglis-dev/py-gpt   #
 # MIT License                                        #
 # Created By  : Marcin Szczygliński                  #
-# Updated Date: 2024.11.05 23:00:00                  #
+# Updated Date: 2024.11.07 23:00:00                  #
 # ================================================== #
 
 class TabsDebug:
@@ -22,8 +22,12 @@ class TabsDebug:
     def update(self):
         """Update debug window."""
         self.window.core.debug.begin(self.id)
+        self.window.core.debug.add(self.id, 'current PID', str(self.window.controller.ui.tabs.get_current_pid()))
+        self.window.core.debug.add(self.id, 'current IDX', str(self.window.controller.ui.tabs.get_current_idx()))
+        self.window.core.debug.add(self.id, 'current Type', str(self.window.controller.ui.tabs.get_current_type()))
         self.window.core.debug.add(self.id, 'last_pid', str(self.window.core.tabs.last_pid))
         self.window.core.debug.add(self.id, 'count(pids)', str(len(self.window.core.tabs.pids)))
+        self.window.core.debug.add(self.id, 'count(ctx bags)', str(len(self.window.core.ctx.container.bags)))
         self.window.core.debug.add(self.id, '----', '')
 
         # PIDs
@@ -36,7 +40,4 @@ class TabsDebug:
         self.window.core.debug.add(self.id, 'PID => meta.id', str(self.window.core.ctx.output.mapping))
         self.window.core.debug.add(self.id, '(last) PID => meta.id', str(self.window.core.ctx.output.last_pids))
         self.window.core.debug.add(self.id, '(last) PID', str(self.window.core.ctx.output.last_pid))
-
-        self.window.core.debug.add(self.id, '----', '')
-
         self.window.core.debug.end(self.id)
