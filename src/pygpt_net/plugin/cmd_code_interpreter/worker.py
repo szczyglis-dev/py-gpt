5f64e5723eae338817f74606992b7d6b497cf507
Marcin Szczyglinski <szczyglis@protonmail.com>
2024-11-17 16:50:20 +0100

    Removed unused print

--- src/pygpt_net/plugin/cmd_code_interpreter/worker.py
+++ src/pygpt_net/plugin/cmd_code_interpreter/worker.py
@@ -6,7 +6,7 @@
 # GitHub:  https://github.com/szczyglis-dev/py-gpt   #
 # MIT License                                        #
 # Created By  : Marcin Szczygliński                  #
-# Updated Date: 2024.11.16 05:00:00                  #
+# Updated Date: 2024.11.17 17:00:00                  #
 # ================================================== #
 
 from PySide6.QtCore import Slot, Signal
@@ -35,7 +35,6 @@ class Worker(BaseWorker):
     @Slot()
     def run(self):
         responses = []
-        print("exe")
         for item in self.cmds:
             try:
                 response = None
