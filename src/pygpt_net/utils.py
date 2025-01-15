3045cf94ba96d56189c15c781f91ac08bf97d814
Marcin Szczyglinski <szczyglis@protonmail.com>
2025-01-16 00:52:51 +0100

    Fix: list parsing

--- src/pygpt_net/utils.py
+++ src/pygpt_net/utils.py
@@ -6,7 +6,7 @@
 # GitHub:  https://github.com/szczyglis-dev/py-gpt   #
 # MIT License                                        #
 # Created By  : Marcin Szczygliński                  #
-# Updated Date: 2024.11.15 02:00:00                  #
+# Updated Date: 2025.01.16 01:00:00                  #
 # ================================================== #
 
 import json
@@ -137,12 +137,21 @@ def parse_args(data: list) -> dict:
                 except Exception:
                     args[key] = False
         elif type == 'dict':
-            try:
-                args[key] = json.loads(value)
-            except:
-                args[key] = {}
+            if isinstance(value, dict):
+                args[key] = value
+            else:
+                try:
+                    args[key] = json.loads(value)
+                except:
+                    args[key] = {}
         elif type == 'list':
-            args[key] = [x.strip() for x in value.split(',')]
+            if isinstance(value, list):
+                args[key] = value
+            else:
+                try:
+                    args[key] = [x.strip() for x in value.split(',')]
+                except:
+                    args[key] = []
         elif type == 'None':
             args[key] = None
         else:
