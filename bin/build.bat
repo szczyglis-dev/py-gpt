1c03350d883a115053fe8e66df0e54fb76ab72b2
Marcin Szczyglinski <szczyglis@protonmail.com>
2025-07-07 18:45:32 +0200

    Updated windows spec

--- bin/build.bat
+++ bin/build.bat
@@ -9,6 +9,14 @@ call venv\Scripts\activate
 call pip install -r requirements.txt
 call pyinstaller --noconfirm windows.spec
 
+REM Copy VC runtime DLLs
+copy "%BuildDir%\_internal\msvcp100.dll" "%BuildDir%\"
+copy "%BuildDir%\_internal\msvcr100.dll" "%BuildDir%\"
+copy "%BuildDir%\_internal\pywin32_system32\pywintypes310.dll" "%BuildDir%\_internal\"
+copy "%BuildDir%\_internal\pywin32_system32\pythoncom310.dll" "%BuildDir%\_internal\"
+copy "%BuildDir%\_internal\pywin32_system32\pywintypes310.dll" "%BuildDir%\"
+copy "%BuildDir%\_internal\pywin32_system32\pythoncom310.dll" "%BuildDir%\"
+
 REM Copy ffmpg
 copy "%BinDir%\ffmpeg.exe" "%BuildDir%\"
 copy "%BinDir%\ffplay.exe" "%BuildDir%\"
