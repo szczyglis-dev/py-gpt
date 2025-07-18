493a032d9c02992524a8c36bbab590371df3b98d
Marcin Szczyglinski <szczyglis@protonmail.com>
2025-07-19 00:56:04 +0200

    Updated version to 2.5.52

--- bin/build_all.bat
+++ bin/build_all.bat
@@ -16,10 +16,10 @@ call build_installer.bat
 
 REM Sign
 IF EXIST "%CD%\..\dist\pygpt-%ProductVersion%.msi" (
-    echo "Installer (download) found, signing..."
+    echo Installer standalone found, signing...
     "%SIGNTOOL%\signtool.exe" sign /t http://time.certum.pl "%CD%\..\dist\pygpt-%ProductVersion%.msi"
 ) ELSE (
-    echo "Installer (download) not found, aborting..."
+    echo Installer standalone not found, aborting...
 )
 
 REM Create installer (MS Store)
@@ -27,8 +27,8 @@ call build_installer_store.bat
 
 REM Sign
 IF EXIST "%CD%\..\dist\store\pygpt-%ProductVersion%.msi" (
-    echo "Installer (MS Store) found, signing..."
+    echo Installer MS Store found, signing...
     "%SIGNTOOL%\signtool.exe" sign /t http://time.certum.pl "%CD%\..\dist\store\pygpt-%ProductVersion%.msi"
 ) ELSE (
-    echo "Installer (MS Store) not found, aborting..."
+    echo Installer MS Store not found, aborting...
 )
