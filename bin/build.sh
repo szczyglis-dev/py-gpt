9e3081f528c0f487f8755355d5edde8b174f8676
Marcin Szczyglinski <szczyglis@protonmail.com>
2025-07-18 22:26:19 +0200

    Updated build script

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # This script is used to build the app using pyinstaller
+# WARNING: minimum PyInstaller version required: 6.4
+# pip install pyinstaller==6.4.0
 
 VERSION="2.5.51"
 
@@ -20,11 +22,19 @@ cp -rf $DIR_PARENT/icon.png $DIR_PARENT/dist/Linux/
 
 mv $DIR_PARENT/dist/Linux $DIR_PARENT/dist/pygpt-$VERSION
 cd $DIR_PARENT/dist
-zip -r pygpt-$VERSION.zip pygpt-$VERSION -9
+
+# tar.gz
+tar -czf pygpt-$VERSION.tar.gz pygpt-$VERSION
+
+# .zip
+zip -r pygpt-$VERSION.zip pygpt-$VERSION.tar.gz -9
 cd $DIR_PARENT
 
-python -m build
-twine check dist/*
+poetry env activate
+poetry build
+
+twine check dist/pygpt_net-$VERSION.tar.gz
+twine check dist/pygpt_net-$VERSION-py3-none-any.whl
 
 if [ -f "$DIR_PARENT/dist/pygpt-$VERSION.zip" ]; then
 	sha1sum $DIR_PARENT/dist/pygpt-$VERSION.zip
